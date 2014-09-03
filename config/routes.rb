5848e9019385bd4ddc5d112ee7cbc225668aaca7
Cullen Jett <cullenjett@gmail.com>
2014-09-03 15:25:39 -0600

    End of week 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
 
-  resources :users, only: [:new, :show, :create]
+  resources :users, only: [:new, :show, :create] do
+    post 'follow' => 'following_relationships#create'
+  end
   resources :shouts, only: [:show]
   resources :text_shouts, only: [:create]
   resources :photo_shouts, only: [:create]
