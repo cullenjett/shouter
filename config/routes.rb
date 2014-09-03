1a7967e89c62520329a0b4681725cfd0ad07a861
Cullen Jett <cullenjett@gmail.com>
2014-09-03 14:47:14 -0600

    Polymorphic shouts - image and photo

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
+
   resources :users, only: [:new, :show, :create]
-  resources :shouts, only: [:create, :show]
+  resources :shouts, only: [:show]
+  resources :text_shouts, only: [:create]
+  resources :photo_shouts, only: [:create]
 end
